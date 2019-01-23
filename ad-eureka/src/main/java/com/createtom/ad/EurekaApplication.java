fc983b52086e831c5a20ee497bcb1b1dfa9fd0b5
huwenjian <hwj_gz@163.com>
2019-01-23 16:48:42 +0800

    #1 eureka done!

---
+++ ad-eureka/src/main/java/com/createtom/ad/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.createtom.ad;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: hwj
+ * @Date: 2019/1/23 16:39
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnableEurekaServer.class, args);
+    }
+}
