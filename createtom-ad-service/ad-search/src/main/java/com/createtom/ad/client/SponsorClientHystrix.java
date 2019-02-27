f242ed5411fd5baa20077f757b8f5cf246076e06
huwenjian <hwj_gz@163.com>
2019-02-27 16:34:36 +0800

    #3：ad-search : 加注注解

--- createtom-ad-service/ad-search/src/main/java/com/createtom/ad/client/SponsorClientHystrix.java
+++ createtom-ad-service/ad-search/src/main/java/com/createtom/ad/client/SponsorClientHystrix.java
@@ -17,6 +17,15 @@ import java.util.List;
 @Component
 public class SponsorClientHystrix implements SponsorClient {
 
+
+    /**
+     * Feign 方式调用
+     * Feign 可以实现声明式的 Web 服务客户端
+     * 通过 @FeignClient 指定调用的服务名称
+     * 在接口上声明 @RequestMapping 指明调用服务的地址与请求类型
+     * 通过在 @FeignClient 中配置 fallback 指定熔断
+     * 实现接口：SponsorClient.java，熔断：SponsorClientHystrix.java
+     */
     @Override
     public CommonResponse<List<AdPlan>> getAdPlans(AdPlanGetRequest request) {
         return new CommonResponse<>(-1, "eureka-client-ad-sponsor error");
